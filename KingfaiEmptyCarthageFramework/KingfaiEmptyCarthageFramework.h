b108f8e0b7b442598a7540967599fc2c7ecad292
Albert So <aso@atlassian.com>
2017-04-28 13:09:05 -0700

    initial checkin

---
+++ KingfaiEmptyCarthageFramework/KingfaiEmptyCarthageFramework.h
@@ -0,0 +1,19 @@
+//
+//  KingfaiEmptyCarthageFramework.h
+//  KingfaiEmptyCarthageFramework
+//
+//  Created by Albert So on 4/27/17.
+//  Copyright © 2017 Albert So. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KingfaiEmptyCarthageFramework.
+FOUNDATION_EXPORT double KingfaiEmptyCarthageFrameworkVersionNumber;
+
+//! Project version string for KingfaiEmptyCarthageFramework.
+FOUNDATION_EXPORT const unsigned char KingfaiEmptyCarthageFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KingfaiEmptyCarthageFramework/PublicHeader.h>
+
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # KingfaiEmptyCarthageFramework
-Empty Carthage Framework for testing
+Empty Carthage Framework for testing carthage_cache
+
+To grab build time stamp:
+/usr/libexec/PlistBuddy -c "Print :CFBuildDate"  ${PATH_TO_INFO.PLIST}
+
